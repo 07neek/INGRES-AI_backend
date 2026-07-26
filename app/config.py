e7591b0ba807ded36c4322e40fbfb2e381938626
07neek <neekjon9@gmail.com>
2026-07-26 13:51:45 +0530

    update

--- app/config.py
+++ app/config.py
@@ -38,7 +38,8 @@ class Settings(BaseSettings):
     # Comma-separated list of origins allowed to call this API from a browser.
     FRONTEND_ORIGINS: str = (
         "http://localhost:3000,http://127.0.0.1:3000,"
-        "http://localhost:5173,http://127.0.0.1:5173"
+        "http://localhost:5173,http://127.0.0.1:5173,"
+        "http://localhost:5500,http://127.0.0.1:5500"
     )
 
     # --- Conversation memory (in-process, no DB) ------------------------
